f2df19dabea29b8c481a3fa6dd927a2fbcff2679
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2022-06-26 17:28:59 +0100

    adding copyright

--- multi_matrix_pca_example/stop_criteria.m
+++ multi_matrix_pca_example/stop_criteria.m
@@ -1,3 +1,25 @@
+% MIT License
+%
+% Copyright (c) 2022 Marcelo Jacinto
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+%
 function [stop, objective_next, diff] = stop_criteria(Xk_prev, Xk_next, s1_prev, s2_prev, s1_next, s2_next, tau_k_prev, tau_k_next, Ai, p, delta)
 
 % Alocating memory for better performance
